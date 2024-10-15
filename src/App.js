f0105ce811d6d91957bc8c013fda050dfc6ecd37
Naveenyarramilli <yarramillinaveen@gmail.com>
2024-10-15 15:24:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+const redButton = () =>{
+ 
+   document.getElementById('myred').style.backgroundColor = "red";
+    document.getElementById('myyellow').style.backgroundColor = "black";
+     document.getElementById('mygreen').style.backgroundColor = "black";
+}
+
+const yellowButton = () =>{
+ 
+  document.getElementById('myred').style.backgroundColor = "black";
+   document.getElementById('myyellow').style.backgroundColor = "yellow";
+    document.getElementById('mygreen').style.backgroundColor = "black";
+}
+
+const greenButton = () =>{
+ 
+  document.getElementById('myred').style.backgroundColor = "black";
+   document.getElementById('myyellow').style.backgroundColor = "black";
+    document.getElementById('mygreen').style.backgroundColor = "green";
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{'color':'white'}}>Traffic lights</h1>
+      <div className='section'>
+        <div className='section-1'>
+          <button onClick={redButton}>Red</button>
+          <button onClick={yellowButton}>Yellow</button>
+          <button onClick={greenButton}>Green</button>
+        </div>
+        <div className='section-2'>
+          <p id="myred"></p>
+          <p id="myyellow"></p>
+          <p id="mygreen"></p>
+
+        </div>
+      </div>
+      
     </div>
   );
 }
